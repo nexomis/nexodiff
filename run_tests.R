f604df00c87041282c190d45129b9591369675fa
jfouret <julien@fouret.me>
2025-06-23 08:55:39 +0000

    fix run tests and add nextflow pipeline outdir in tests

--- run_tests.R
+++ run_tests.R
@@ -1,21 +1,20 @@
 
-temp_lib_path <- "_trash/test_lib"
-if (!dir.exists(temp_lib_path)) {
-  dir.create(temp_lib_path, recursive = TRUE)
-}
-.libPaths(c(temp_lib_path, .libPaths()))
+local({
+  temp_lib_path <- withr::local_tempdir()
+  .libPaths(c(temp_lib_path, .libPaths()))
 
-devtools::install_local(
-  path = ".",
-  force = TRUE,
-  quiet = FALSE,
-  build_manual = FALSE,
-  build_vignettes = FALSE,
-  upgrade = "never",
-  lib = temp_lib_path
-)
+  devtools::install_local(
+    path = ".",
+    force = TRUE,
+    quiet = FALSE,
+    build_manual = FALSE,
+    build_vignettes = FALSE,
+    upgrade = "never",
+    lib = temp_lib_path
+  )
 
-testthat::test_local(
-  ".",
-  load_package = "installed"
-)
+  testthat::test_local(
+    ".",
+    load_package = "installed"
+  )
+})
